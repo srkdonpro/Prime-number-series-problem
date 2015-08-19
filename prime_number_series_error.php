77112fd009df4da07ffd983c01858900da2180af
srkdonpro <srkdonpro@gmail.com>
2015-08-20 05:51:03 +0600

    Prime number problem
    lorem ipsum

---
+++ prime_number_series_error.php
@@ -0,0 +1,84 @@
+<?php
+
+//		Prime Numbers: 1+3+5+7+11+...(total 10 prime number)=?
+$count=0;
+$sum=0;
+
+for($i=1;;$i++){
+	if($count==10){
+		break;
+	}
+	if($i%2==0){
+		continue;
+	}
+	else{
+		if($i==1){
+			$count++;
+			$sum=$sum+$i;
+			echo $i."+";
+		}
+		else{
+			$prime="Checking";
+			for($j=2;$j<$i;$j++){
+				if($i%$j==0){
+					$prime="No";
+					break;
+				}
+			}
+			if(($prime=="Checking")&&($count<10)){
+				$count++;
+				$sum=$sum+$i;
+				echo $i."+";
+			}
+			elseif(($prime=="Checking")&&($count==10)){
+				$count++;
+				$sum=$sum+$i;
+				echo $i."=";
+			}
+		}
+	}
+}
+echo $sum;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
